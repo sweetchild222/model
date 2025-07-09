a5004871799fbc344180b3cc716e6870cf945e89
crazygun22 <crazygun22@nate.com>
2025-07-09 13:03:11 +0900

    update

---
+++ seq2seq_attention/decoder.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+class Decoder(tf.keras.Model):
+  
+  def __init__(self, vocab_size, embedding_dim, units, batch_size, attention):
+    super(Decoder, self).__init__()
+
+    self.batch_size = batch_size
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
+    self.fc = tf.keras.layers.Dense(vocab_size)
+    self.attention = attention
+
+
+  def call(self, x, hidden, output):
+
+    context_vector, attention_weights = self.attention(hidden, output)
+
+    x = self.embedding(x)
+    x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+    output, state = self.gru(x)
+    output = tf.reshape(output, (-1, output.shape[2]))
+
+    x = self.fc(output)
+
+    return x, state, attention_weights
+
+
---
+++ seq2seq_attention/encoder.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+class Encoder(tf.keras.Model):
+
+  def __init__(self, vocab_size, embedding_dim, units, batch_size):
+    super(Encoder, self).__init__()
+
+    self.batch_size = batch_size
+    self.units = units
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(self.units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
+
+  def call(self, x, hidden):
+
+    x = self.embedding(x)
+  
+    output, state = self.gru(x, initial_state = hidden)
+
+    return output, state
+
+  def initialize_hidden_state(self):
+    
+    return tf.zeros((self.batch_size, self.units))
+
+
+
