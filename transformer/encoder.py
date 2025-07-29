75ff4a7d1dc1a447286f064e13b7d09d724be223
crazygun22 <crazygun22@nate.com>
2025-07-29 10:46:28 +0900

    update

--- transformer/encoder.py
+++ transformer/encoder.py
@@ -7,7 +7,7 @@ def encoder_layer(dff, d_model, num_heads, dropout, name="encoder_layer"):
   
     inputs = tf.keras.Input(shape=(None, d_model), name="inputs")  
     padding_mask = tf.keras.Input(shape=(1, 1, None), name="padding_mask")
-  
+
     attention = MultiHeadAttention(d_model, num_heads, name="attention")({
             'query': inputs, 'key': inputs, 'value': inputs, 'mask': padding_mask})
 
@@ -17,18 +17,17 @@ def encoder_layer(dff, d_model, num_heads, dropout, name="encoder_layer"):
     # Position-wise FFNN
     outputs = tf.keras.layers.Dense(units=dff, activation='relu')(attention)
     outputs = tf.keras.layers.Dense(units=d_model)(outputs)
-
-    # Drop out + residual learning
+    
     outputs = tf.keras.layers.Dropout(rate=dropout)(outputs)
-    outputs = tf.keras.layers.LayerNormalization(epsilon=1e-6)(attention + outputs)
+    outputs = tf.keras.layers.LayerNormalization(epsilon=1e-6)(attention + outputs) # Residual connection
 
     return tf.keras.Model(inputs=[inputs, padding_mask], outputs=outputs, name=name)
 
 
 def encoder(vocab_size, num_layers, dff, d_model, num_heads, dropout, name="encoder"):
-    
+
     inputs = tf.keras.Input(shape=(None,), name="inputs")
-  
+
     padding_mask = tf.keras.Input(shape=(1, 1, None), name="padding_mask")
 
     embeddings = tf.keras.layers.Embedding(vocab_size, d_model)(inputs)
