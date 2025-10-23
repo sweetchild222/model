942b8e8bc486b61e06ce38eba879fdde850da67d
crazygun22 <crazygun22@nate.com>
2025-10-23 14:01:49 +0900

    update

--- bert/model/transformer/transformer_encorder.py
+++ bert/model/transformer/transformer_encorder.py
@@ -10,21 +10,21 @@ class TransformerEncorder(nn.Module):
         super().__init__()
         
         self.attention = MultiHeadAttention(head_count=attention_head_count, d_model=hidden)
-        self.attention_residual_norm = nn.LayerNorm(hidden)
+        self.attention_norm = nn.LayerNorm(hidden)
         self.dropout = nn.Dropout(p=dropout)
         self.feed_forward = PositionwiseFeedForward(d_model=hidden, d_ff=feed_forward_hidden, dropout=dropout)
-        self.feed_forward_residual_norm = nn.LayerNorm(hidden)
+        self.feed_forward_norm = nn.LayerNorm(hidden)
         
     def forward(self, x, mask=None):
 
-        x = x + self.attention.forward(query=x, key=x, value=x, mask=mask)
+        x = x + self.attention.forward(query=x, key=x, value=x, mask=mask) #attention and residual connection
 
-        x = self.attention_residual_norm.forward(x)
+        x = self.attention_norm.forward(x)
         
         x = self.dropout.forward(x)
 
-        x = x + self.feed_forward.forward(x)
+        x = x + self.feed_forward.forward(x) #feed_forward and residual connection
         
-        x = self.feed_forward_residual_norm.forward(x)
+        x = self.feed_forward_norm.forward(x)
     
         return x
